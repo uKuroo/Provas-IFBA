2d67f4213f3714c9846550bc956b11da47ac1d67
Gabriel Nascimento <opitimalracer50@hotmail.com>
2024-08-23 00:25:17 -0300

    Coisa boa

---
+++ AV3_2023.2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int base, altura, area;
+
+    scanf("%d %d",&base,&altura);
+    area = base * altura;
+
+    printf("\nArea %d",area);
+
+    return 0;
+}
