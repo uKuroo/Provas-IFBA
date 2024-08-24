251cd9e0350d326a35e29cdb27b310ea243a20b1
Gabriel Nascimento <opitimalracer50@hotmail.com>
2024-08-24 20:26:34 -0300

    AV3 2019 inacabado

---
+++ AV3_2019.1 Q1.c
@@ -0,0 +1,45 @@
+/*Uma faixa de letras é um conjunto de letras minúsculas alfabeticamente consecutivas tomadas
+de 'a' até 'z'. A menor e maior letras da faixa, separadas por dois pontos (o caractere ':'), são
+usadas para representar a faixa de letras. Por exemplo, a faixa "a:c" representa as letras
+consecutivas 'a', 'b' e 'c'. (as aspas não fazem parte da faixa). A faixa "w:z" representa as letras
+'w', 'x', 'y' e 'z'. A faixa "m:m" representa apenas a letra 'm'. 
+Escreva um programa em C, que leia
+uma string composta por letras minúsculas e espaços em branco e imprima as faixas de letras
+que a compõe, ordenadas alfabeticamente pelo menor valor de cada faixa. Faixas de letras a
+serem impressas devem representar a maior sequência possível de letras de entrada em ordem
+crescente. Ignore espaços e letras duplicadas contidas na entrada.
+Por exemplo, a string "iron maiden" tem cinco faixas de letras, "a:a" (a letra 'a'), "d:e" (as letras
+'d' e 'e'), "i:i" (a letra 'i'), "m:o" (as letras 'm', 'n' e 'o') e "r:r" (a letra 'x'). Assim a saída do
+programa deveria ser: "a:a d:e i:i m:o r:r".*/
+
+#include <stdio.h>
+#define string 50
+
+int main(){
+    char frase[string],ordem[string], aux;
+    int icount, jcount;
+    
+    printf("Insira uma frase com apenas letras minusculas e espacos\n");
+    fgets(frase,50,stdin);
+
+    for(jcount=0;jcount < 50;jcount++){
+        for(icount=0;frase[icount+1] != '\n';icount++){
+                if(frase[icount] > frase[icount+1]){
+                aux = frase[icount + 1];
+                frase[icount + 1] = frase[icount];
+                frase[icount] = aux;
+            }
+        }
+    }
+    for(icount=0,jcount=0;frase[icount] != '\n';icount++){
+        if(frase[icount] > ' '){
+            ordem[jcount] = frase[icount];
+            jcount++;
+        }
+    }
+
+    puts(frase);
+    puts(ordem);
+
+    return 0;
+}
