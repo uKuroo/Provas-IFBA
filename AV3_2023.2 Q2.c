d16c06bac0715b3f7d1da2be14de3761afc478a4
Gabriel Nascimento <opitimalracer50@hotmail.com>
2024-08-24 18:07:29 -0300

    AV3 2023.2 Q2

---
+++ AV3_2023.2 Q2.c
@@ -0,0 +1,34 @@
+/*Palavras como “Belo”, “fiNo”, “CrUz” e “FLor” possuem a interessante propriedade
+de que suas letras aparecem todas em ordem alfabética. Por exemplo, em “FLor”, F vem antes de L
+no alfabeto, L vem antes de o, e o vem antes de r. Dizemos que palavras com essa propriedade são
+ordenadas.
+Escreva um programa em C, que leia uma palavra e determine se ela é uma palavra ordenada.*/
+#include <stdio.h>
+#define string 256
+
+int main()
+{
+    int icount, ordem = 0;
+    char palavra[string];
+
+    printf("Bota a palavra ai bro namoral\n");
+    fgets(palavra,256,stdin);
+
+    for(icount = 0;palavra[icount] != '/n' && palavra[icount] != '\0';icount++){
+        if(palavra[icount] >= 'A' && palavra[icount] <= 'Z')
+            palavra[icount]+=32;
+    }
+
+    for(icount=0;palavra[icount + 1]!='\n';icount++){
+        if(palavra[icount] > palavra[icount+1])
+            ordem++;
+    }
+
+    if(ordem > 0){
+        printf("Palavra desordenada.");
+    }else{
+        printf("Palavra ordenada.");
+    }
+
+    return 0;
+}
