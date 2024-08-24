fe7d5f2833aaf3612eb652d54fe0327aa733aa78
Gabriel Nascimento <opitimalracer50@hotmail.com>
2024-08-24 18:10:44 -0300

    AV2 2015.2 Q3

--- AV2_2015.2 Q3.c
+++ AV2_2015.2 Q3.c
@@ -1,3 +1,11 @@
+/*Bruninho está programando um personagem virtual para o próximo desafio de um jogo
+de aventura em que, numa das fases, o personagem tem que entrar em um corredor,
+percorrer algumas salas e depois sair do corredor. Ele pode entrar apenas uma vez, e
+passar por cada sala apenas uma vez. Todas as salas possuem uma porta de entrada e
+uma de saída. Ao passar por uma sala o jogador ganha um certo número de vidas (que
+pode ser negativo!). O objetivo é passar pelo corredor coletando a maior quantidade
+possível de vidas! Por sorte, sempre existe ao menos uma sala onde se ganha um
+número positivo de vidas.*/
 #include <stdio.h>
 #define TAM 10
 int main()
